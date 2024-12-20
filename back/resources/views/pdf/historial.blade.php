cf2190b4e4fb64fd9cab7cfcd38c9b4d066f0f1b
adimer <adimer101@gmail.com>
2024-12-20 05:12:09 -0400

    Actulizado para linea2

--- back/resources/views/pdf/historial.blade.php
+++ back/resources/views/pdf/historial.blade.php
@@ -68,6 +68,19 @@
         .w-100 {
             width: 100px;
         }
+        .border{
+            border: 1px solid #000;
+            padding: 0;
+            margin: 3px;
+            border-radius: 5px;
+        }
+        .circle {
+            width: 100px;
+            height: 100px;
+            margin: 3px;
+            border-radius: 50%;
+            border: 1px solid #000;
+        }
     </style>
 </head>
 <body>
@@ -82,7 +95,7 @@
         N° H.CL: {{ $historial->id }}
     </div>
 </div>
-<div style="border: 1px solid #000;text-align: left;padding: 0;margin: 0;border-radius: 5px;">
+<div class="border">
     <table style="border: 0;padding: 0;margin: 0;">
         <tr>
             <td class="center lineHeight" style="border: 0" >
@@ -109,7 +122,177 @@
         </tr>
     </table>
 </div>
+<table style="border: 0;padding: 0;margin: 0;">
+    <tr>
+        <td class="lineHeight" style="border: 0" >
+            <div class="border" style="padding: 3px">
+                Fecha de Nacimiento: {{ $historial->paciente->fecha_nacimiento }}
+            </div>
+        </td>
+        <td class="lineHeight" style="border: 0" >
+            <div class="border" style="padding: 3px">
+                Sexo:
+                <span class="{{ $historial->paciente->sexo == 'M' ? 'circle' : '' }}">
+                    M
+                </span>
+                <span class="{{ $historial->paciente->sexo == 'F' ? 'circle' : '' }}">
+                    F
+                </span>
+            </div>
+        </td>
+        <td class="lineHeight" style="border: 0" >
+            <div class="border" style="padding: 3px">
+                Estado Civil: {{ $historial->paciente->estado_civil }}
+            </div>
+        </td>
+    </tr>
+</table>
 
+
+{{--{--}}
+{{--"id": 1,--}}
+{{--"nombre": "Marcos",--}}
+{{--"apellido": "Carreón",--}}
+{{--"fecha_nacimiento": "1983-04-27",--}}
+{{--"identificacion": "624079258",--}}
+{{--"edad": 72,--}}
+{{--"sexo": "F",--}}
+{{--"estado_civil": "Soltero",--}}
+{{--"direccion": "Camino Alejandro, 37, Ático 7º, 79494, O Meléndez de las Torres",--}}
+{{--"telefono": "915342948",--}}
+{{--"fecha_creacion": "2024-11-16 06:28:18",--}}
+{{--"nombre_completo": "Marcos Carreón",--}}
+{{--"historial_medicos": [--}}
+{{--{--}}
+{{--"id": 1,--}}
+{{--"paciente_id": 1,--}}
+{{--"user_id": 1,--}}
+{{--"fecha": "2004-01-05 23:00:57",--}}
+{{--"referido_de": "Oliva-Cervantes y Asoc.",--}}
+{{--"motivo_consulta": "Porro ab sunt eum quidem.",--}}
+{{--"enfermedad_actual": "Voluptatem natus itaque quas.",--}}
+{{--"alergias_conocidas": "Unde itaque ut id qui.",--}}
+{{--"user": {--}}
+{{--"id": 1,--}}
+{{--"name": "Adminstrador",--}}
+{{--"username": "admin",--}}
+{{--"email": "admin@test.com",--}}
+{{--"role": "Doctor",--}}
+{{--"color": "orange"--}}
+{{--}--}}
+{{--},--}}
+{{--{--}}
+{{--"id": 2,--}}
+{{--"paciente_id": 1,--}}
+{{--"user_id": 1,--}}
+{{--"fecha": "2024-11-16 00:00:00",--}}
+{{--"referido_de": null,--}}
+{{--"motivo_consulta": null,--}}
+{{--"enfermedad_actual": null,--}}
+{{--"alergias_conocidas": null,--}}
+{{--"user": {--}}
+{{--"id": 1,--}}
+{{--"name": "Adminstrador",--}}
+{{--"username": "admin",--}}
+{{--"email": "admin@test.com",--}}
+{{--"role": "Doctor",--}}
+{{--"color": "orange"--}}
+{{--}--}}
+{{--},--}}
+{{--{--}}
+{{--"id": 3,--}}
+{{--"paciente_id": 1,--}}
+{{--"user_id": 1,--}}
+{{--"fecha": "2024-11-16 00:00:00",--}}
+{{--"referido_de": "asda",--}}
+{{--"motivo_consulta": "asda",--}}
+{{--"enfermedad_actual": "adasd",--}}
+{{--"alergias_conocidas": "adasdsa",--}}
+{{--"user": {--}}
+{{--"id": 1,--}}
+{{--"name": "Adminstrador",--}}
+{{--"username": "admin",--}}
+{{--"email": "admin@test.com",--}}
+{{--"role": "Doctor",--}}
+{{--"color": "orange"--}}
+{{--}--}}
+{{--},--}}
+{{--{--}}
+{{--"id": 4,--}}
+{{--"paciente_id": 1,--}}
+{{--"user_id": 1,--}}
+{{--"fecha": "2024-11-16 00:00:00",--}}
+{{--"referido_de": "referido de",--}}
+{{--"motivo_consulta": "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",--}}
+{{--"enfermedad_actual": "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",--}}
+{{--"alergias_conocidas": "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",--}}
+{{--"user": {--}}
+{{--"id": 1,--}}
+{{--"name": "Adminstrador",--}}
+{{--"username": "admin",--}}
+{{--"email": "admin@test.com",--}}
+{{--"role": "Doctor",--}}
+{{--"color": "orange"--}}
+{{--}--}}
+{{--},--}}
+{{--{--}}
+{{--"id": 5,--}}
+{{--"paciente_id": 1,--}}
+{{--"user_id": 1,--}}
+{{--"fecha": "2024-11-16 07:59:00",--}}
+{{--"referido_de": "referido del doctor",--}}
+{{--"motivo_consulta": "motivo de la contata",--}}
+{{--"enfermedad_actual": "asda",--}}
+{{--"alergias_conocidas": "asdas",--}}
+{{--"user": {--}}
+{{--"id": 1,--}}
+{{--"name": "Adminstrador",--}}
+{{--"username": "admin",--}}
+{{--"email": "admin@test.com",--}}
+{{--"role": "Doctor",--}}
+{{--"color": "orange"--}}
+{{--}--}}
+{{--}--}}
+{{--],--}}
+{{--"signos_vitales": [--}}
+{{--{--}}
+{{--"id": 1,--}}
+{{--"paciente_id": 1,--}}
+{{--"estado_general": "Enim distinctio molestiae quo qui vitae nam blanditiis.",--}}
+{{--"fc": 90,--}}
+{{--"fr": 16,--}}
+{{--"pa": 95,--}}
+{{--"temperatura": "35.03",--}}
+{{--"peso": "119.78",--}}
+{{--"talla": "2.09",--}}
+{{--"imc": "16.57",--}}
+{{--"spo2": 93,--}}
+{{--"glasgow": 5--}}
+{{--}--}}
+{{--],--}}
+{{--"antecedentes_familiares": [--}}
+{{--{--}}
+{{--"id": 1,--}}
+{{--"paciente_id": 1,--}}
+{{--"tuberculosis": 1,--}}
+{{--"diabetes": 1,--}}
+{{--"hipertension": 0,--}}
+{{--"cardiopatia": 0--}}
+{{--}--}}
+{{--],--}}
+{{--"habitos_personales": [--}}
+{{--{--}}
+{{--"id": 1,--}}
+{{--"paciente_id": 1,--}}
+{{--"fuma": 0,--}}
+{{--"alcohol": 1,--}}
+{{--"drogas": 1,--}}
+{{--"zoonosis": 0,--}}
+{{--"deportes": 1,--}}
+{{--"vacunas": 1--}}
+{{--}--}}
+{{--]--}}
+{{--}--}}
 <div class="section font-style-script">
     <div class="font-style-script">Datos del Paciente</div>
     <table>
