631456157e11458065b9f0ffc41aef7e5e742313
adimer <adimer101@gmail.com>
2025-04-08 06:26:50 -0400

    creciaciond eimagenes

--- back/database/migrations/2024_11_13_204102_create_productos_table.php
+++ back/database/migrations/2024_11_13_204102_create_productos_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('productos', function (Blueprint $table) {
             $table->id();
             $table->string('nombre')->nullable();
+            $table->string('imagen')->nullable();
             $table->text('descripcion')->nullable();
             $table->string('unidad')->nullable();
             $table->decimal('precio', 10, 2)->nullable();
