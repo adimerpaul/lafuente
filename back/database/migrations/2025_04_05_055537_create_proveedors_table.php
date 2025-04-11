48654b709772a8503434f906cc23f1b7e9c84890
adimer <adimer101@gmail.com>
2025-04-11 06:25:55 -0400

    actilizado para proveores

--- back/database/migrations/2025_04_05_055537_create_proveedors_table.php
+++ back/database/migrations/2025_04_05_055537_create_proveedors_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('Proveedores', function (Blueprint $table) {
+        Schema::create('proveedores', function (Blueprint $table) {
             $table->id();
             $table->string('nombre')->nullable();
             $table->string('ci')->nullable();
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('Proveedores');
+        Schema::dropIfExists('proveedores');
     }
 };
